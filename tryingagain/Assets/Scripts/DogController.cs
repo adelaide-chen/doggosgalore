1a059ae8dceecb369b85ade4c70ece60817051e4
Stanley Chen <ada.chen@berkeley.edu>
2018-07-04 10:04:07 -0700

    all dem files??

---
+++ tryingagain/Assets/Scripts/DogController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+public class DogController : MonoBehaviour {
+
+    public enum DogState { Chase, Retrieve };
+    public enum AnimatorTransition { idleToRun, runToIdle, runToDrink, drinkToRun };
+    public DogState state;
+    public float stopDistance = 1.0f;
+    public Transform ballTransform;
+    public Transform headTransform;
+
+    Animator animator;
+    NavMeshAgent agent;
+    
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+
+        // Code to trigger action on startup
+        if (state == DogState.Chase) {
+            Fetch();
+        }
+        else if (state == DogState.Retrieve) {
+            Retrieve();
+        }
+    }
+	
+	void FixedUpdate () {
+        float distance = Vector3.Distance(transform.position, agent.destination);
+        Debug.Log(distance);
+        if (distance < stopDistance || distance == float.PositiveInfinity) {
+            agent.enabled = false;
+            if (state == DogState.Chase) {
+                Debug.Log("Run to drink (chase)");
+                animator.SetInteger("state", (int)AnimatorTransition.runToDrink);
+            }
+            else if (state == DogState.Retrieve) {
+                Debug.Log("Run to idle (retrieve)");
+                animator.SetInteger("state", (int)AnimatorTransition.runToIdle);
+            }
+        }
+        else {
+            agent.enabled = true;
+            if (state == DogState.Chase) {
+                Debug.Log("Idle to run (chase)");
+                animator.SetInteger("state", (int)AnimatorTransition.idleToRun);
+            }
+            else if (state == DogState.Retrieve) {
+                Debug.Log("Idle to run (retrieve)");
+                animator.SetInteger("state", (int)AnimatorTransition.idleToRun);
+            }
+        }
+	}
+
+    public void Fetch() {
+        state = DogState.Chase;
+        agent.destination = ballTransform.position;
+    }
+
+    public void Retrieve() {
+        state = DogState.Retrieve;
+        agent.destination = headTransform.position;
+    }
+}
---
+++ tryingagain/Assets/Scripts/TheFetchOfDoggo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheFetchOfDoggo : MonoBehaviour {
+
+    private Animator doggo;
+    private MoveTo pupper;
+
+	// Use this for initialization
+	void Start () {
+        doggo = GetComponent<Animator>();
+        pupper = doggo.GetComponent<MoveTo>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (pupper.isMoving)
+        {
+            doggo.SetBool("isChasing", true);
+            print("chasingggg");
+        } else
+        {
+            doggo.SetBool("isChasing", false);
+        }
+
+        if (Vector3.Distance(pupper.transform.position, pupper.goal.transform.position) < 1.0f)
+        {
+            doggo.SetBool("isChasing", false);
+            pupper.isMoving = false;
+            doggo.SetBool("isReached", true);
+            doggo.SetBool("isRetrieved", true);
+            print("reached and retrieved!!");
+
+        } else
+        {
+            doggo.SetBool("isReached", false);
+            doggo.SetBool("isRetrieved", false);
+        }
+	}
+}
