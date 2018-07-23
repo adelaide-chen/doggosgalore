a862e04b0313ccb24413419a48edef8d0ccb2dda
unknown <ada.chen@berkeley.edu>
2018-07-23 16:53:32 -0700

    oculus

---
+++ dogulus/Assets/Scripts/BallGrab.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallGrab : RWVR_SimpleGrab {
+
+    public DogController dogController;
+
+    public override void OnTriggerWasPressed(RWVR_InteractionController controller)
+    {
+        base.OnTriggerWasPressed(controller);
+        dogController.Stop();
+    }
+
+    public override void OnTriggerWasReleased(RWVR_InteractionController controller)
+    {
+        base.OnTriggerWasReleased(controller);
+        dogController.Go();
+    }
+
+}
---
+++ dogulus/Assets/Scripts/DogController.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Animator))]
+[System.Serializable]
+public class DogController : MonoBehaviour
+{
+
+    public enum DogState { Chase, Retrieve };
+    public enum AnimatorTransition { idleToRun, runToIdle, runToDrink, drinkToRun, idleToDrink, drinkToIdle };
+    public float ballStopDistance;
+    public float playerStopDistance;
+    public Transform mouth;
+    public Transform player;
+    public Transform ball;
+
+    Animator animator;
+    NavMeshAgent agent;
+    Rigidbody ballRB;
+    Transform target;
+    bool hasBall;
+
+    float animPercent
+    {
+        get
+        {
+            AnimatorStateInfo animatorInfo = animator.GetCurrentAnimatorStateInfo(0);
+            return animatorInfo.normalizedTime;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+        ballRB = ball.GetComponent<Rigidbody>();
+        target = ball;
+        hasBall = false;
+    }
+
+    void FixedUpdate()
+    {
+        agent.destination = target.position;
+
+        //float distance = CalculatePathDistance(agent);
+        float distance = Vector3.Distance(transform.position, target.position);
+        // Change this later!
+        float xzDistance = Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(target.position.x, target.position.z));
+
+        if (target == ball)
+        {
+            if (distance < ballStopDistance)
+            {
+                agent.isStopped = true;
+                if (animator.GetCurrentAnimatorStateInfo(0).IsName("PugRun"))
+                {
+                    animator.SetInteger("state", (int)AnimatorTransition.runToDrink);
+                }
+                else if (animator.GetCurrentAnimatorStateInfo(0).IsName("PugIdle"))
+                {
+                    animator.SetInteger("state", (int)AnimatorTransition.idleToDrink);
+                }
+                if (animPercent >= .3f && animator.GetCurrentAnimatorStateInfo(0).IsName("PugDrink"))
+                {
+                    target = player;
+                    hasBall = true;
+                    ballRB.isKinematic = true;
+                    agent.isStopped = false;
+                }
+            }
+            else
+            {
+                agent.isStopped = false;
+                animator.SetInteger("state", (int)AnimatorTransition.idleToRun);
+            }
+        }
+        else if (target == player)
+        {
+            if (xzDistance < playerStopDistance)
+            {
+                agent.isStopped = true;
+                if (animator.GetCurrentAnimatorStateInfo(0).IsName("PugRun"))
+                {
+                    animator.SetInteger("state", (int)AnimatorTransition.runToIdle);
+                }
+                else if (animator.GetCurrentAnimatorStateInfo(0).IsName("PugDrink"))
+                {
+                    animator.SetInteger("state", (int)AnimatorTransition.drinkToIdle);
+                }
+            }
+            else
+            {
+                agent.isStopped = false;
+                animator.SetInteger("state", (int)AnimatorTransition.drinkToRun);
+            }
+            if (hasBall)
+            {
+                ball.position = mouth.position;
+                ball.rotation = mouth.rotation;
+            }
+        }
+        else
+        {
+            // Do nothing
+            animator.SetInteger("state", (int)AnimatorTransition.runToIdle);
+        }
+
+    }
+
+    public void Stop()
+    {
+        target = transform;
+        ballRB.isKinematic = false;
+        hasBall = false;
+    }
+
+    public void Go()
+    {
+        target = ball;
+    }
+
+    private void FaceTarget(Vector3 destination)
+    {
+        Vector3 lookPos = destination - transform.position;
+        lookPos.y = 0;
+        Quaternion rotation = Quaternion.LookRotation(lookPos);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 10f);
+    }
+
+    /*private float CalculatePathDistance(NavMeshAgent agent)
+    {
+        NavMeshPath path = agent.path;j lkgfd;sajfd;lskafj;dlska
+
+        float distance = 0f;
+        Vector3[] corners = path.corners;
+        for (int c = 0; c < corners.Length - 1; ++c)
+        {
+            distance += Mathf.Abs((corners[c] - corners[c + 1]).magnitude);
+        }
+        return distance;
+    }*/
+}
